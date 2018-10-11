b41f94223415a1aef9744d60b24e693c3faad50a
MayBeTall <eric.r.fraze@gmail.com>
2018-10-11 09:58:14 -0400

    Thumb tracking, better finger curl script, hang-ten gesture

---
+++ Scripts/FingerCurler.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+public class FingerCurler : MonoBehaviour {
+    public SteamVR_Behaviour_Skeleton skeleton;
+
+    public float IndexCurl = 0f;
+    public float MiddleCurl = 0f;
+    public float RingCurl = 0f;
+    public float PinkyCurl = 0f;
+    public float ThumbCurl = 0f;
+
+    private float IndexMinDistance = 10000f, IndexMaxDistance = 0f;
+    private float MiddleMinDistance = 10000f, MiddleMaxDistance = 0f;
+    private float RingMinDistance = 10000f, RingMaxDistance = 0f;
+    private float PinkyMinDistance = 10000f, PinkyMaxDistance = 0f;
+    private float ThumbMinDistance = 10000f, ThumbMaxDistance = 0f;
+
+    private float IndexAng, MiddleAng, RingAng, PinkyAng, ThumbAng;
+
+    void Start () {
+        skeleton.SetRangeOfMotion(Valve.VR.EVRSkeletalMotionRange.WithoutController);
+    }
+	
+	void Update () {
+		IndexAng =  skeleton.GetBoneRotation(SteamVR_Skeleton_JointIndexes.indexProximal, true).eulerAngles.z;
+		IndexMinDistance = Mathf.Min(IndexMinDistance, IndexAng);
+		IndexMaxDistance = Mathf.Max(IndexMaxDistance, IndexAng);
+        IndexCurl = Mathf.InverseLerp(IndexMaxDistance, IndexMinDistance, IndexAng);
+
+		MiddleAng = skeleton.GetBoneRotation(SteamVR_Skeleton_JointIndexes.middleProximal, true).eulerAngles.z;
+		MiddleMinDistance = Mathf.Min(MiddleMinDistance, MiddleAng);
+		MiddleMaxDistance = Mathf.Max(MiddleMaxDistance, MiddleAng);
+        MiddleCurl = Mathf.InverseLerp(MiddleMaxDistance, MiddleMinDistance, MiddleAng);
+
+		RingAng = skeleton.GetBoneRotation(SteamVR_Skeleton_JointIndexes.ringProximal, true).eulerAngles.z;
+		RingMinDistance = Mathf.Min(RingMinDistance, RingAng);
+		RingMaxDistance = Mathf.Max(RingMaxDistance, RingAng);
+        RingCurl = Mathf.InverseLerp(RingMaxDistance, RingMinDistance, RingAng);
+
+		PinkyAng = skeleton.GetBoneRotation(SteamVR_Skeleton_JointIndexes.pinkyProximal, true).eulerAngles.z;
+		PinkyMinDistance = Mathf.Min(PinkyMinDistance, PinkyAng);
+		PinkyMaxDistance = Mathf.Max(PinkyMaxDistance, PinkyAng);
+        PinkyCurl = Mathf.InverseLerp(PinkyMaxDistance, PinkyMinDistance, PinkyAng);
+
+		ThumbAng = skeleton.GetBoneRotation(SteamVR_Skeleton_JointIndexes.thumbProximal, true).eulerAngles.z;
+		ThumbMinDistance = Mathf.Min(ThumbMinDistance, ThumbAng);
+		ThumbMaxDistance = Mathf.Max(ThumbMaxDistance, ThumbAng);
+        ThumbCurl = Mathf.InverseLerp(ThumbMaxDistance, ThumbMinDistance, ThumbAng);
+    }
+}
--- Scripts/KnucklePoses.cs
+++ Scripts/KnucklePoses.cs
@@ -9,112 +9,43 @@ namespace MayBeKnuckles {
 		[System.Serializable]
 		public class Pose {
 			public string name;
-			public enum thumbControls {Unused, Off, A_Button, B_Button, Track_Button, Thumbstick};
-			public thumbControls thumb;
-			[Range(-1, 100)]
+			[Range(-0.01f, 1)]
+			public float thumb;
+			[Range(-0.01f, 1)]
 			public float index;
-			[Range(-1, 100)]
+			[Range(-0.01f, 1)]
 			public float middle;
-			[Range(-1, 100)]
+			[Range(-0.01f, 1)]
 			public float ring;
-			[Range(-1, 100)]
+			[Range(-0.01f, 1)]
 			public float pinky;
 		}
 
-		[System.Serializable]
-		public class DebugPose {
-			public enum thumbControls {Off, A_Button, B_Button, Track_Button, Thumbstick};
-			public thumbControls thumb;
-			[Range(-1, 100)]
-			public float index;
-			[Range(-1, 100)]
-			public float middle;
-			[Range(-1, 100)]
-			public float ring;
-			[Range(-1, 100)]
-			public float pinky;
-		}
-
-		public class Finger {
-			public SteamVR_Behaviour_Skeleton hand;
-			public int jointIndex;
-			public float min;
-			public float max;
-			public float current;
-			public float value;
-
-			public Finger(int jointIndex) {
-				this.jointIndex = jointIndex;
-			}
-
-			public void update() {
-				current = hand.skeletonRoot.InverseTransformPoint( hand.GetBonePosition( jointIndex ) ).z;
-
-				if (current > 0) {
-					min = Mathf.Min(min, current);
-					max = Mathf.Max(max, current);
-					value = ((current - min) * 100) / (max - min);
-				}
-
-				// Reset min if it bugs out and gets an impossible value.
-				if (min == 0) {
-					min = 999;
-				}
-			}
-		}
-		
-		public Finger[] fingers = new Finger[4];
-
-		public SteamVR_Behaviour_Skeleton hand;
 		public Pose[] poses;
 		public Pose currentPose;
-		public DebugPose debugPose;
-		
-		public Finger index = new Finger(SteamVR_Skeleton_JointIndexes.indexTip);
-		public Finger middle = new Finger(SteamVR_Skeleton_JointIndexes.middleTip);
-		public Finger ring = new Finger(SteamVR_Skeleton_JointIndexes.ringTip);
-		public Finger pinky = new Finger(SteamVR_Skeleton_JointIndexes.pinkyTip);
 
-		private float waitForInit = 1;
+		private FingerCurler fingerCurler;
+
 		// Use this for initialization
 		void Start () {
-			SteamVR_Behaviour_Skeleton hand = GetComponent<SteamVR_Behaviour_Skeleton>();
-			fingers[0] = index;
-			fingers[1] = middle;
-			fingers[2] = ring;
-			fingers[3] = pinky;
-
-			foreach(Finger finger in fingers ) {
-				finger.hand = hand;
-			}
+			fingerCurler = GetComponent<FingerCurler>();
 		}
 		
 		// Update is called once per frame
 		void Update () {
 			currentPose = null;
 
-			waitForInit -= Time.deltaTime;
-			if ( waitForInit < 0 ) {
-				foreach(Finger finger in fingers ) {
-					finger.update();
+			foreach(Pose pose in poses) {
+				bool testIndex = Mathf.Abs(pose.index - fingerCurler.IndexCurl) < 0.20 || pose.index < 0;
+				bool testMiddle = Mathf.Abs(pose.middle - fingerCurler.MiddleCurl) < 0.20 || pose.middle < 0;
+				bool testRing = Mathf.Abs(pose.ring - fingerCurler.RingCurl) < 0.20 || pose.ring < 0;
+				bool testPinky = Mathf.Abs(pose.pinky - fingerCurler.PinkyCurl) < 0.20|| pose.pinky < 0;
+				bool testThumb = Mathf.Abs(pose.thumb - fingerCurler.ThumbCurl) < 0.20|| pose.thumb < 0;
+
+				if ( testIndex && testMiddle && testRing && testPinky && testThumb ) {
+					currentPose = pose;
+					return;
 				}
-
-				foreach(Pose pose in poses) {
-					bool testIndex = Mathf.Abs(pose.index - index.value) < 10;
-					bool testMiddle = Mathf.Abs(pose.middle - middle.value) < 10;
-					bool testRing = Mathf.Abs(pose.ring - ring.value) < 10;
-					bool testPinky = Mathf.Abs(pose.pinky - pinky.value) < 10;
-
-					if ( testIndex && testMiddle && testRing && testPinky ) {
-						currentPose = pose;
-						return;
-					}
-				}
-
-				debugPose.index = index.value;
-				debugPose.middle = middle.value;
-				debugPose.ring = ring.value;
-				debugPose.pinky = pinky.value;
 			}
 		}
 	}
