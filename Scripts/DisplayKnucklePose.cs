159595bf5eedaffc84f57bce57ace9f2ca75221a
MayBeTall <eric.r.fraze@gmail.com>
2018-10-11 07:13:17 -0400

    Include script

---
+++ Scripts/DisplayKnucklePose.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using MayBeKnuckles;
+
+public class DisplayKnucklePose : MonoBehaviour {
+	private TextMesh text;
+	public KnucklePoses pose;
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( pose.currentPose != null ) {
+			text.text = pose.currentPose.name;
+		} else {
+			text.text = "No pose";
+		}
+	}
+}
