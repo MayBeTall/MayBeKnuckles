3bf72449b8813dbd63463f5c4e92bb551b74cb8b
MayBeTall <eric.r.fraze@gmail.com>
2018-10-12 04:43:09 -0400

    Added some magic

---
+++ Scripts/KnuckleMagic.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+namespace MayBeKnuckles {
+	/*
+	 * An example class that combines SteamVR actions
+	 * and KnucklePoses.
+	 */
+	public class KnuckleMagic : MonoBehaviour {
+		public KnucklePoses poses;
+		private SteamVR_Input_Sources hand;
+
+		[SteamVR_DefaultActionSet("magic")]
+        public SteamVR_ActionSet actionSet;
+
+        [SteamVR_DefaultAction("Squeeze")]
+        public SteamVR_Action_Single cast;
+
+		public GameObject fingerTipEffectPrefab;
+		private GameObject fingerTipEffect;
+		private SpiritGunGlow fingerTipGlow;
+		public float fingerTipRotationYOffset;
+
+		private Transform fingerTip;
+
+		private float castForce;
+		private float spiritGunCooldown;
+
+
+		// Use this for initialization
+		void Start () {
+			actionSet.ActivateSecondary();
+			hand = GetComponent<SteamVR_Behaviour_Skeleton>().inputSource;
+			fingerTip = GetComponent<SteamVR_Behaviour_Skeleton>().indexTip;
+			if ( fingerTipEffectPrefab != null ) {
+				fingerTipEffect = Instantiate(fingerTipEffectPrefab, fingerTip.position, fingerTip.rotation);
+				fingerTipGlow = fingerTipEffect.GetComponent<SpiritGunGlow>();
+				fingerTipGlow.target = fingerTip;
+				fingerTipGlow.snap.localEulerAngles = new Vector3(fingerTipGlow.snap.localEulerAngles.x, fingerTipRotationYOffset, fingerTipGlow.snap.localEulerAngles.z);
+			}
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			castForce = cast.GetAxis( hand );
+			if ( spiritGunCooldown > 0 ) {
+				spiritGunCooldown -= Time.deltaTime;
+			}
+			
+			if ( castForce > 0.1f && poses.currentPose != null ) {
+				if ( poses.currentPose.name == "finger-gun" && spiritGunCooldown <= 0 ) {
+					fingerTipGlow.fadeIn();
+					if ( castForce > 0.7f ) {
+						fingerTipGlow.fire();
+					}
+				} else {
+					fingerTipGlow.fadeOut();
+				}
+			} else {
+				fingerTipGlow.fadeOut();
+			}
+		}
+	}
+}
---
+++ Scripts/Magic/SpiritGunGlow.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritGunGlow : MonoBehaviour {
+	private float scale = 0.0f;
+	private bool fadingOut = false;
+	public ParticleSystem glowParticles;
+	public ParticleSystem blastParticles;
+	public Transform snap;
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+		scale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target != null) {
+			this.transform.position = target.position;
+			this.transform.rotation = target.rotation;
+		}
+		if ( fadingOut ) {
+			if ( glowParticles.isEmitting ) {
+				glowParticles.Stop();
+			}
+			if ( scale > 0 ) {
+				scale -= 1f * Time.deltaTime;
+			}
+		} else {
+			if ( ! glowParticles.isEmitting ) {
+				glowParticles.Play();
+			}
+			if ( scale < 1 ) {
+				scale += 1f * Time.deltaTime;
+			}
+		}
+
+		transform.localScale = new Vector3(scale, scale, scale);
+	}
+
+	public void fadeOut() {
+		fadingOut = true;
+	}
+	public void fadeIn() {
+		fadingOut = false;
+	}
+	public void fire() {
+		if (scale >= 0.9) {
+			blastParticles.Emit(1);
+			scale = 0;
+		}
+	}
+}
